4f5f9e83dfd115ffa885272e34f7b1c89a972b13
ilis137 <iliyasshahapure1@gmail.com>
2023-01-17 14:54:04 +0530

    initial commit

---
+++ src/test/java/com/employeepayrollapp/EmployeePayrollAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeepayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
